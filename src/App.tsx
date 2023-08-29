b53ada983de57cd405dba01e48c90699b4df7b65
LilSem <evgenyi_sit@mail.ru>
2023-08-30 01:14:21 +0300

    Добавил Prettier в проект

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Привет, Команда!</h1>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
