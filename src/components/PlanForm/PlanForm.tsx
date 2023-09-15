fe30556102ebee40dd2fb5dddb22b0bb574f6686
LilSem <evgenyi_sit@mail.ru>
2023-09-15 22:51:36 +0300

    feat: закончил работу с NavBar.tsx

--- src/components/PlanForm/PlanForm.tsx
+++ src/components/PlanForm/PlanForm.tsx
@@ -32,45 +32,44 @@ const PlanForm = ({ textTitle, namePlan, data }: { textTitle: string; namePlan: 
   });
   return (
     <>
-    <div className={styles.plan__content}>
-      <TitleBlock text={textTitle} isBack={true} />
-      <h1 className={styles.plan__userData}>Никитина Александра Сергеевна, 35 лет</h1>
-      <form className={styles.plan__form} onSubmit={onSubmit}>
-        <label className={styles.plan__label}>
-          <h3 className={styles.plan__title}>{`Название плана ${namePlan}`}</h3>
-          <input
-            className={styles.plan__input}
-            type="text"
-            {...register('namePlan', {
-              required: 'Поле не должно быть пустым',
-            })}
-            placeholder="Напишите название плана"
-            // required
-          />
-        </label>
-        {location.pathname === '/planMeal' && <CaloriesInput register={register} />}
-        <label className={styles.plan__label}>
-          <h3 className={styles.plan__title}>Рекомендации</h3>
-          <textarea
-            className={`${styles.plan__input} ${styles.plan__input_textarea}`}
-            {...register('recomendations', {
-              required: 'Поле не должно быть пустым',
-            })}
-            placeholder="Напишите рекомендации"
-          />
-        </label>
-        <div className={styles.plan__label_gap}>
-          {data.map((item, index) => (
-            <DayBlock item={item} key={index} />
-          ))}
-        </div>
+      <div className={styles.plan__content}>
+        <TitleBlock text={textTitle} isBack={true} />
+        <h1 className={styles.plan__userData}>Никитина Александра Сергеевна, 35 лет</h1>
+        <form className={styles.plan__form} onSubmit={onSubmit}>
+          <label className={styles.plan__label}>
+            <h3 className={styles.plan__title}>{`Название плана ${namePlan}`}</h3>
+            <input
+              className={styles.plan__input}
+              type="text"
+              {...register('namePlan', {
+                required: 'Поле не должно быть пустым',
+              })}
+              placeholder="Напишите название плана"
+              // required
+            />
+          </label>
+          {location.pathname === '/planMeal' && <CaloriesInput register={register} />}
+          <label className={styles.plan__label}>
+            <h3 className={styles.plan__title}>Рекомендации</h3>
+            <textarea
+              className={`${styles.plan__input} ${styles.plan__input_textarea}`}
+              {...register('recomendations', {
+                required: 'Поле не должно быть пустым',
+              })}
+              placeholder="Напишите рекомендации"
+            />
+          </label>
+          <div className={styles.plan__label_gap}>
+            {data.map((item, index) => (
+              <DayBlock item={item} key={index} />
+            ))}
+          </div>
 
-        <Button textBtn="Сохранить" type="submit" isDirty={isDirty} isValid={isValid} />
-      </form>
-      <ButtonCancel text="Отменить" isDirty={isDirty} isValid={isValid} />
-      <NavBar/>
-    </div>
-
+          <Button textBtn="Сохранить" type="submit" isDirty={isDirty} isValid={isValid} />
+        </form>
+        <ButtonCancel text="Отменить" isDirty={isDirty} isValid={isValid} />
+        <NavBar statusSpec={true} />
+      </div>
     </>
   );
 };
