35b38ce27d34bab4d075318e3d060042f382f6ab
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-09-16 18:25:31 +0300

    refactor: Убирает resetPasswordTooltip заменяет его на более универсальный confirmationTooltip

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Modal.module.scss';
 
 type Props = {
-  children?: React.ReactNode;
+  children: React.ReactNode;
 };
 
 export default function Modal({ children }: Props) {
