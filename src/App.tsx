77a6ea0d7e9e52ef72e0ba0404e7a57e295e8c7e
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-09-17 13:42:53 +0300

    промежуточный коммит

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import './scss/app.scss';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/Navbar/NavBar.tsx';
-import Login from './pages/AuthPages/Login';
-import Register from './pages/AuthPages/Register';
-import ChangePassword from './pages/AuthPages/ChangePassword';
-import AuthByEmail from './pages/AuthPages/AuthByEmail.tsx';
-import PasswordSaved from './components/PasswordSaved';
 import LandingPage from './pages/LandingPage/LandingPage.tsx';
 import Profile from './pages/ProfilePage/Profile.tsx';
 
@@ -29,11 +24,6 @@ function App() {
     <>
       <div className="App">
         <Routes>
-          <Route path="signup" element={<Register />} />
-          <Route path="signin" element={<Login />} />
-          <Route path="password" element={<ChangePassword />} />
-          <Route path="signin-netw" element={<AuthByEmail />} />
-          <Route path="saved" element={<PasswordSaved />} />
           <Route path="/" element={<LandingPage />} />
           <Route path="/nutrition" element={<LandingPage />} />
           <Route path="/trainer" element={<LandingPage />} />
