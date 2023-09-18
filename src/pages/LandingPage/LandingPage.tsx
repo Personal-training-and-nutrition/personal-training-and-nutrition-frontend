dcb21e3411a290293cb93564abe737f5db390314
LilSem <evgenyi_sit@mail.ru>
2023-09-19 00:53:25 +0300

    feat: Верстка Toggle.tsx и обновление лого в проекте

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,56 +1,29 @@
 import React from 'react';
 import styles from './LandingPage.module.scss';
-import img from '../../assets/images/cardItem/bxs-camera-plus.svg';
-import CardItem from '../../components/LandingPage/CardItem/CardItem.tsx';
 import Toggle from '../../components/LandingPage/Toggle/Toggle.tsx';
 import logo from '../../assets/logo.svg';
-
-type ItemsType = {
-  title: string;
-  subtitle: string;
-  imageUrl: string;
-  imageAlt: string;
-  isReverse: boolean;
-};
-
-const items: ItemsType[] = [
-  {
-    title: 'Удобный сервис для тренеров, диетологов, нутрициологов',
-    subtitle: 'Составление планов тренировок и питания, онлайн-поддержка клиентов в одном сервисе',
-    imageUrl: img,
-    imageAlt: 'картинка',
-    isReverse: false,
-  },
-  {
-    title: 'Отслеживайте прогресс',
-    subtitle: 'Организовывайте и храните информацию о приёмах в одном месте',
-    imageUrl: img,
-    imageAlt: 'картинка',
-    isReverse: true,
-  },
-  {
-    title: 'Отправляйте рекомендации',
-    subtitle: 'Организовывайте и храните информацию о приёмах в одном месте',
-    imageUrl: img,
-    imageAlt: 'картинка',
-    isReverse: false,
-  },
-];
+import oneImg from '../../assets/images/landingPage/users/one.png';
 
 const LandingPage: React.FC = () => {
   return (
-    <main className={styles.landing__container}>
-      <img className={styles.landing__logo} src={logo} alt="Логотип сайта" />
-      <Toggle />
-      <section className={styles.landing__content}>
-        <div className={styles.button__small}></div>
-        <ul className={styles.landing__list}>
-          {items.map((item, index) => {
-            return <CardItem key={index} {...item} />;
-          })}
-        </ul>
-      </section>
-    </main>
+    <div className={styles.landing}>
+      <header className={styles.landing__header}>
+        <img className={styles.landing__headerLogo} src={logo} alt="Логотип сайта" />
+        <Toggle />
+      </header>
+      <main>
+        <div className={styles.landing__itemsContainer}>
+          <h2 className={styles.landing__itemsContainerTitle}>
+            Удобный сервис для&nbsp;
+            <span className={styles.landing__itemsContainerTitleBlue}>тренеров, диетологов, нутрициологов</span>
+          </h2>
+          <p className={styles.landing__itemsContainerSubtitle}>
+            Составление планов тренировок и&nbsp;питания, онлайн-поддержка клиентов&nbsp;в одном сервисе
+          </p>
+          <img className={styles.landing__itemsContainerImage} src={oneImg} alt="Первое изображение" />
+        </div>
+      </main>
+    </div>
   );
 };
 
