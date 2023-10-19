4cf954c86b154da69007188a5858119b9c07812b
Mariya <zma611@yandex.ru>
2023-10-19 11:56:54 +0300

    fix: отображение на старнице Планы питания - планов текущего клиента

--- src/pages/MealPlans/MealPlans.tsx
+++ src/pages/MealPlans/MealPlans.tsx
@@ -4,34 +4,31 @@ import styles from './MealPlans.module.scss';
 import planImage from '../../assets/mealPlanImage.png';
 import { Link } from 'react-router-dom';
 import { useGetDietPlansListQuery } from '../../redux/services/dietApi';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
+import { useAppSelector } from '../../redux/store';
 
 function MealPlans() {
   const { data, isSuccess } = useGetDietPlansListQuery();
+  const currentClient = useAppSelector((state) => state.currentClient.client);
 
-  const cards = data?.map((plan) => (
-    <Link to={`/meal-plan?id=${plan.id}`} className={styles.mealPlans__link} key={plan.id}>
-      <PlanCard title={plan.name || 'Без названия'} date="Hell knows when" image={planImage} />
-    </Link>
-  ));
+  const plansCurrentClient = data?.map(
+    (plan) => plan.user === currentClient.id && (
+      <Link to={`/meal-plan?id=${plan.id}`} className={styles.mealPlans__link} key={plan.id}>
+        <PlanCard title={plan.name || 'Без названия'} date={plan.describe || ''} image={planImage} />
+      </Link>
+    ),
+  );
 
-  useEffect(() => {
-    if (isSuccess) console.log(data);
-  }, [isSuccess]);
+  // useEffect(() => {
+  //   if (isSuccess) console.log(data);
+  // }, [isSuccess]);
 
   return (
     <main className="App__container">
       <div className={styles.mealPlans}>
         <TitleBlock text="планы питания" />
         <div className={styles.mealPlans__list}>
-          {isSuccess && cards}
-          {/* <Link to="/meal-plan" className={styles.mealPlans__link}>
-            <PlanCard title="Минус 2кг (1 неделя)" date="Создан 28 сентября 2023" image={planImage} />
-          </Link>
-
-          <Link to="/meal-plan" className={styles.mealPlans__link}>
-            <PlanCard title="Минус 2кг (1 неделя)" date="Создан 28 сентября 2023" image={planImage} />
-          </Link> */}
+          {isSuccess && plansCurrentClient}
         </div>
       </div>
     </main>
